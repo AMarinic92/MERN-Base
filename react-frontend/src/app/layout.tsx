72b326616283c053893c51ffef911b4790453b2d
AMarinic92 <andrew.marinic92@gmail.com>
2025-11-22 15:36:40 -0600

    - generic side nav added. - need to do routing

--- react-frontend/src/app/layout.tsx
+++ react-frontend/src/app/layout.tsx
@@ -1,18 +1,25 @@
-import React from 'react';
-import { Tabs } from '#/ui/tabs';
-import db from '#/lib/db';
-Tabs
-export default async function Layout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  const demo = db.demo.find({ where: { slug: 'route-groups' } });
-  const sections = db.section.findMany({ limit: 1 });
+import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
+import { AppSidebar } from "@/components/app-sidebar"
+import "@/app/globals.css"  // or "./globals.css" depending on your structure
 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-      
-
-      <div>{children}</div>
-  );
+    <html lang="en">
+      <body>
+        <SidebarProvider>
+          <div id='main-header'className="flex min-h-screen border-8 border-amber-400">
+            <AppSidebar />
+            <main className="flex-1 border-8 border-green-500">
+              <div className="border-8 border-red-500">
+                <SidebarTrigger />
+              </div>
+              <div className="border-8 bg-amber-800 border-blue-500 p-4">
+                {children}
+              </div>
+            </main>
+          </div>
+        </SidebarProvider>
+      </body>
+    </html>
+  )
 }
