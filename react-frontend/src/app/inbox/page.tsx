2f8f61ec3f82d39f365b1ab88dff58575062075b
AMarinic92 <andrew.marinic92@gmail.com>
2025-11-23 17:05:35 -0600

    - base front end is done.

---
+++ react-frontend/src/app/inbox/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Inbox() {
+  return (
+    <div className="flex min-h-screen items-center justify-center  font-sans dark:bg-black">
+      <main className="flex min-h-screen w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
+        <h1>INBOX</h1>
+      </main>
+    </div>
+  );
+}
--- react-frontend/src/app/layout.tsx
+++ react-frontend/src/app/layout.tsx
@@ -1,23 +1,32 @@
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/components/app-sidebar"
 import "@/app/globals.css"  // or "./globals.css" depending on your structure
+import Image from "next/image"
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body>
+
         <SidebarProvider>
-          <div id='main-header'className="flex min-h-screen border-8 border-amber-400">
             <AppSidebar />
-            <main className="flex-1 border-8 border-green-500">
-              <div className="border-8 border-red-500">
+            <main className="flex-1">
+              <div  className=" flex flex-row gap-x-2.5 border-8 border-red-500 w-full">
                 <SidebarTrigger />
+                <Image
+                  className="dark:invert border-amber-100 border-2"
+                  src="/next.svg"
+                  alt="Next.js logo"
+                  width={100}
+                  height={10}
+                  priority
+                />
+                
               </div>
-              <div className="border-8 bg-amber-800 border-blue-500 p-4">
+              <div className="border-8 border-blue-500 p-4">
                 {children}
               </div>
             </main>
-          </div>
         </SidebarProvider>
       </body>
     </html>
